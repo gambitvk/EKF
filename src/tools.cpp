273c529e315a2a2d9375469cdabdf76691035345
gambit <gambitvk@hotmail.com>
2017-04-26 08:29:06 +1000

    tools done...started working on kalman_filter.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -10,16 +10,55 @@ Tools::Tools() {}
 Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+                              const vector<VectorXd> &ground_truth) 
+{
+    VectorXd rmse(4);
+    rmse << 0,0,0,0;
+
+    if(estimations.size() == 0 ||
+       estimations.size() != ground_truth.size())
+    {
+        std::cout << "Invalid estimation or ground_truth data" << std::endl;
+        return rmse;
+    }
+
+    for(unsigned int i=0; i < estimations.size() ; ++i)
+    {
+        VectorXd res = estimations[i] - ground_truth[i];
+
+        res = res.array() * res.array();
+        rmse += res;
+    }
+
+    rmse = rmse/estimations.size();
+    return rmse.array().sqrt();
 }
 
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state)
+{
+    MatrixXd Hj(3,4);
+    //recover state parameters
+    float px = x_state(0);
+    float py = x_state(1);
+    float vx = x_state(2);
+    float vy = x_state(3);
+
+    //check division by zero
+
+    if(px*py*vx*vy == 0)
+    {
+        std::cout << "CalculateJacobian () - Error - Division by Zero" << std::endl; 
+        return Hj;
+    }
+    //compute the Jacobian matrix
+    Hj(2,2) = Hj(0,0) = px / sqrt(cal1(px,py));
+    Hj(2,3) = Hj(0,1) = py / sqrt(cal1(px,py));
+
+    Hj(1,0) = -py / cal1(px,py);
+    Hj(1,1) = px / cal1(px,py);
+
+    Hj(2,0) = cal2(py,px,vx,vy) / pow(cal1(px,py),3/2);
+    Hj(2,1) = cal2(px,py,vy,vx) / pow(cal1(px,py),3/2);
+
+    return Hj;
 }
--- src/tools.h
+++ src/tools.h
@@ -21,10 +21,29 @@ public:
   Eigen::VectorXd CalculateRMSE(const std::vector<Eigen::VectorXd> &estimations, const std::vector<Eigen::VectorXd> &ground_truth);
 
   /**
+   *
   * A helper method to calculate Jacobians.
   */
   Eigen::MatrixXd CalculateJacobian(const Eigen::VectorXd& x_state);
 
+private:
+    template <class s>
+        s cal1 (const s &s1, const s &s2);
+
+    template <class s>
+        s cal2(const s &p1,const s &p2, const s &v1 , const s &v2);
 };
+
+template <class s>
+s Tools::cal1(const s &s1,const s &s2)
+{
+        return pow(s1,2) + pow(s2,2);
+}
+
+template <class s>
+s Tools::cal2(const s &p1,const s &p2, const s &v1 , const s &v2)
+{
+        return p1*(v1*p1 - v2*p2);
+}
 
 #endif /* TOOLS_H_ */
